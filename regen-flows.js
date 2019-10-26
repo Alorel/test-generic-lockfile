77e7c1fdb8b34d80fc2039c06a8eabd88a83aa8b
Arturas <a.molcanovas@gmail.com>
2019-10-26 18:22:52 +0100

    regenerate flows

--- regen-flows.js
+++ regen-flows.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const {join} = require('path');
 
-const rev = '7ffc2e940fa116dca5c05517a002d5d45c54a013';
+const rev = '390c7def6e8c01989763c2391e66f12f60f9a185';
 const dir = join(__dirname, '.github', 'workflows');
 
 if (!fs.existsSync(dir)) {
